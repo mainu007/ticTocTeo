e28addfd7588933816e59c95c472e1a8d71f025f
mainu007 <mainudev.co@gmail.com>
2020-04-25 21:05:40 +0600

    Updates

---
+++ precache-manifest.9152186151dff7f42280e3a82e1f3e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf74004b1ae45f55f343696855cda96",
+    "url": "/ticTocTeo/index.html"
+  },
+  {
+    "revision": "a97709148a6c002d9621",
+    "url": "/ticTocTeo/static/css/main.05b09e3f.chunk.css"
+  },
+  {
+    "revision": "f65d7550fcc7719b6dd6",
+    "url": "/ticTocTeo/static/js/2.4cd78aef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTocTeo/static/js/2.4cd78aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97709148a6c002d9621",
+    "url": "/ticTocTeo/static/js/main.56056070.chunk.js"
+  },
+  {
+    "revision": "7a4028a6a3041510ce35",
+    "url": "/ticTocTeo/static/js/runtime-main.d3286b4a.js"
+  }
+]);
